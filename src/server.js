d4eeee705a66e90b925546c17982ba0f1db41577
feril <ferilferdian4@gmail.com>
2025-11-23 16:33:11 +0700

    update: package.json

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
     port: 4000,
-    host: process.env.NODE_ENV !== "production" ? "0.0.0.0" : "0.0.0.0",
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
         origin: ["https://notesapp-v1.dicodingacademy.com"],
