0ef3e2c85a80871382cbd685e1aecc95c5503bf7
feril <ferilferdian4@gmail.com>
2025-12-23 12:29:46 +0700

    fix: server.js

--- src/server.js
+++ src/server.js
@@ -3,36 +3,41 @@ const Hapi = require("@hapi/hapi");
 const Jwt = require("@hapi/jwt");
 require("dotenv").config();
 
-// Exceptions
+// Error
 const ClientError = require("./exceptions/ClientError");
 
-// Music
+// notes
 const notes = require("./api/notes");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
 
-// Users
+// users
 const users = require("./api/users");
-const UsersService = require("./services/database/UsersService"); // Pastikan path benar
+const UsersService = require("./services/postgres/UsersService ");
 const UsersValidator = require("./validator/users");
 
-// Authentications
+// authentications
 const authentications = require("./api/authentications");
-const AuthenticationsService = require("./services/database/AuthenticationsService");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
 const TokenManager = require("./tokenize/TokenManager");
 const AuthenticationsValidator = require("./validator/authentications");
 
-// Collaborations
+// collaborations
 const collaborations = require("./api/collaborations");
-const CollaborationsService = require("./services/database/CollaborationsService");
+const CollaborationsService = require("./services/postgres/CollaborationsService");
 const CollaborationsValidator = require("./validator/collaborations");
 
-//exports
+// exports
 const _exports = require("./api/exports");
 const ProducerService = require("./services/rabbitmq/ProducerService");
 const ExportsValidator = require("./validator/exports");
 
 const init = async () => {
+  const notesService = new NotesService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+  const collaborationsService = new CollaborationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -43,14 +48,12 @@ const init = async () => {
     },
   });
 
-  // Registrasi plugin eksternal
   await server.register([
     {
       plugin: Jwt,
     },
   ]);
 
-  // Mendefinisikan strategi autentikasi jwt
   server.auth.strategy("notesapp_jwt", "jwt", {
     keys: process.env.ACCESS_TOKEN_KEY,
     verify: {
@@ -67,13 +70,6 @@ const init = async () => {
     }),
   });
 
-  // Instansiasi Service
-  const notesService = new NotesService();
-  const usersService = new UsersService();
-  const authenticationsService = new AuthenticationsService();
-  const collaborationsService = new CollaborationsService();
-
-  // Registrasi Plugin Internal
   await server.register([
     {
       plugin: notes,
@@ -102,7 +98,7 @@ const init = async () => {
       plugin: collaborations,
       options: {
         collaborationsService,
-        playlistsService,
+        notesService,
         validator: CollaborationsValidator,
       },
     },
@@ -124,17 +120,6 @@ const init = async () => {
         message: response.message,
       });
       newResponse.code(response.statusCode);
-      return newResponse;
-    }
-
-    // Penanganan error server internal (Opsional tapi disarankan agar tidak bocor infonya)
-    if (response instanceof Error && !response.isBoom) {
-      console.error(response); // Log error asli ke console server
-      const newResponse = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
-      });
-      newResponse.code(500);
       return newResponse;
     }
 
